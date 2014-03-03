333c060e3f7e26e9ac964b798b89c2de630f7220
aniketroxx <aniketp563@gmail.com>
2014-03-03 23:42:23 +0530

    Minor ramdisk packing script glitches

--- make-ramdisk.sh
+++ make-ramdisk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ZPACKER="/home/aniket/Desktop/SUPER_PACKER/Z_PACKER"
+ZPACKER="/home/aniket/Documents/SUPER_PACKER/Z_PACKER"
 
 cd RECOVERY_RAMDISK
 
@@ -8,5 +8,17 @@ find . | cpio --quiet -o -H newc | lzma -qe > ../ramdisk.img
 
 echo "done seems"
 
+read ans
 
+cd ..
+
+cd $ZPACKER
+
+sh clean.sh
+
+cd ..
+
+cp ramdisk.img $ZPACKER
+
+echo "Copied LOL"
 
